0a3de477337c1974fd90900ec3ee0575c9f38226
Jasom Park <zeipar@gmail.com>
2020-09-12 08:16:11 -0700

    Explore argument data type and return type of a function with typescript.

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 console.log("hello");
 const name = "Jason", age = 33, gender = "male";
 const sayHello = (name, age, gender) => {
-    console.log(`hello ${name}, you are ${age}, and you are a ${gender}.`);
+    return `hello ${name}, you are ${age}, and you are a ${gender}.`;
 };
-sayHello(name, age);
+// protects number of arguments
+console.log(sayHello(name, age, gender));
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -3,12 +3,12 @@ const name = "Jason",
   age = 33,
   gender = "male";
 
-const sayHello = (name, age, gender) => {
-  console.log(`hello ${name}, you are ${age}, and you are a ${gender}.`);
+const sayHello = (name: string, age: number, gender: string): string => {
+  return `hello ${name}, you are ${age}, and you are a ${gender}.`;
 };
 
 // protects number of arguments
-sayHello(name, age, gender);
+console.log(sayHello(name, age, gender));
 
 // put '?' at the end of an argument to take undefined as a value.
 // gender?
