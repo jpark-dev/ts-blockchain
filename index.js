914e75a44a5572f4bcbb8ca65e9deda6255e3216
Jasom Park <zeipar@gmail.com>
2020-09-12 08:12:21 -0700

    explore basic typescript rule with argument numbers.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 console.log("hello");
+const name = "Jason", age = 33, gender = "male";
+const sayHello = (name, age, gender) => {
+    console.log(`hello ${name}, you are ${age}, and you are a ${gender}.`);
+};
+sayHello(name, age);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
 console.log("hello");
+const name = "Jason",
+  age = 33,
+  gender = "male";
+
+const sayHello = (name, age, gender) => {
+  console.log(`hello ${name}, you are ${age}, and you are a ${gender}.`);
+};
+
+// protects number of arguments
+sayHello(name, age, gender);
+
+// put '?' at the end of an argument to take undefined as a value.
+// gender?
+
+// show this willbe used as a module.
+export {};
