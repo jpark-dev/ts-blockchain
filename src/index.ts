7369f41b94e6d7c1f6c079734a5aa7f5fa5231e7
Jasom Park <zeipar@gmail.com>
2020-09-14 21:30:58 -0700

    Added class with a constructor so that the types are now compiled into js file.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+// interface Human {
+//   name: string;
+//   age: number;
+//   gender: string;
+// }
+
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
 const person = {
@@ -10,12 +21,14 @@ const person = {
   gender: "male",
 };
 
+const amy = new Human("Amy", 29, "female");
+
 const sayHello = (person: Human): string => {
   return `hello ${person.name}, you are ${person.age}, and you are a ${person.gender}.`;
 };
 
 // protects number of arguments
-console.log(sayHello(person));
+console.log(sayHello(amy));
 
 // put '?' at the end of an argument to take undefined as a value.
 // gender?
