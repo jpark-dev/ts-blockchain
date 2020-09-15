c64f5b5cd2ecd11ffa9e31bd2ba0cb55c852ccc2
Jasom Park <zeipar@gmail.com>
2020-09-14 21:37:39 -0700

    Created new index.ts and added a class for block and displayed the result.

---
+++ src/index copy.ts
@@ -0,0 +1,37 @@
+// interface Human {
+//   name: string;
+//   age: number;
+//   gender: string;
+// }
+
+// class Human {
+//   public name: string;
+//   public age: number;
+//   public gender: string;
+//   constructor(name: string, age: number, gender: string) {
+//     this.name = name;
+//     this.age = age;
+//     this.gender = gender;
+//   }
+// }
+
+// const person = {
+//   name: "Jason",
+//   age: 33,
+//   gender: "male",
+// };
+
+// const amy = new Human("Amy", 29, "female");
+
+// const sayHello = (person: Human): string => {
+//   return `hello ${person.name}, you are ${person.age}, and you are a ${person.gender}.`;
+// };
+
+// // protects number of arguments
+// console.log(sayHello(amy));
+
+// // put '?' at the end of an argument to take undefined as a value.
+// // gender?
+
+// // show this willbe used as a module.
+// export {};
--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,28 @@
-// interface Human {
-//   name: string;
-//   age: number;
-//   gender: string;
-// }
-
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  constructor(name: string, age: number, gender: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+class Block {
+  public index: number;
+  public hash: string;
+  public prevHash: string;
+  public data: string;
+  public timeStamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    prevHash: string,
+    data: string,
+    timeStamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.prevHash = prevHash;
+    this.data = data;
+    this.timeStamp = timeStamp;
   }
 }
 
-const person = {
-  name: "Jason",
-  age: 33,
-  gender: "male",
-};
+const originalBlock = new Block(0, "202020202020202", "", "Hello", 123456);
 
-const amy = new Human("Amy", 29, "female");
+let blockChain: [Block] = [originalBlock];
 
-const sayHello = (person: Human): string => {
-  return `hello ${person.name}, you are ${person.age}, and you are a ${person.gender}.`;
-};
+console.log(blockChain);
 
-// protects number of arguments
-console.log(sayHello(amy));
-
-// put '?' at the end of an argument to take undefined as a value.
-// gender?
-
-// show this willbe used as a module.
 export {};
